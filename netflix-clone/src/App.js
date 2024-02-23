a7b9c51ba710bd13708d02f05675aa31dc7c1e56
Fraol Demisse <fraolashebir84@gmail.com>
2024-02-23 20:08:35 +0300

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
 
+import Home from './Pages/Home/Home';
+
 function App() {
   return (
-    <div >
-      <h1>Hello</h1>
+    <div className='App' >
+      <Home/>
+    
+      
+      
     </div>
   );
 }
---
+++ netflix-clone/src/components/Header/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Header.css";
+import NetflixLogo from "../../assets/images/NetflixLogo.png"
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+const Header = () => {
+  return (
+    <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} alt="Netflix Logo" width="100" />
+            </li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>Mylist</li>
+            <li>Browse by language</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li><AccountBoxIcon/></li>
+            <li><ArrowDropDownIcon/></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

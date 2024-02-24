8750f7bc1948d4196535a69fba42869668c72fe4
Fraol Demisse <fraolashebir84@gmail.com>
2024-02-24 22:01:18 +0300

    banner

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Banner from '../../components/Banner/Banner';
+
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Banner />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <Footer />
     </div>
   );
